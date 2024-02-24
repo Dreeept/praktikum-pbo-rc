2a306df04cf94e2c0207c67cb8a182d22065a3ed
Dreeept <145269436+Dreeept@users.noreply.github.com>
2024-02-24 15:20:15 +0700

    Update 122140194_Andre Philip Tampubolon_Pset{1}.py

--- 122140194_Andre Philip Tampubolon_Pset{1}.py
+++ 122140194_Andre Philip Tampubolon_Pset{1}.py
@@ -1,12 +1,12 @@
 height = int(input("Height = "))
 for i in range(height):
-    for j in range(i, height, 1):
+    for j in range(i, height, 1): #bikin segitiga siku-siku kosong di bagian kiri
         print(" ", end="")
 
-    for k in range(-1,i,1):
+    for k in range(-1,i,1): #bikin segitiga siku-siku untuk bagian kiri dari segitiga sama kaki
         print("*", end="")
 
-    for l in range(0,i,1):
+    for l in range(0,i,1): #bikin segitiga siku-siku untuk bagian kanan dari segitiga sama kaki
         print("*", end="")
     else:
         print(" ")
